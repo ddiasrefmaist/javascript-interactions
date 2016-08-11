bea28737782bf768c1ddd1cc00555925974d29f4
System Administrator <root@Diegos-iMac.local>
2016-08-11 18:09:24 -0300

    distribute version compiled sucessfull

--- gulpfile.js
+++ gulpfile.js
@@ -5,47 +5,61 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	rename = require('gulp-rename'),
 	sass = require('gulp-sass'),
-	maps = require('gulp-sourcemaps');
+	maps = require('gulp-sourcemaps'),
+	del = require('del'),
 	cleanCSS = require('gulp-clean-css');
 
 
 gulp.task ("concatScripts", function() {
-	gulp.src([
-		'js/aberto/jquery.js',
-		'js/aberto/jquery-ui.min.js',
-		'js/aberto/bootstrap.min.js',
-		'js/aberto/jquery.bxslider.js',
-		'js/aberto/modernizr.js',
-		'js/aberto/jquery.tcycle.js',
-		'js/aberto/slick.min.js'
+return gulp.src([
+		'js/jquery.js',
+		'js/jquery-ui.min.js',
+		'js/bootstrap.min.js',
+		'js/jquery.bxslider.js',
+		'js/modernizr.js',
+		'js/jquery.tcycle.js',
+		'js/slick.min.js'
 	])
 	.pipe(maps.init())
-	.pipe(concat("piped.js"))
+	.pipe(concat('piped.js'))
 	.pipe(maps.write('./'))
-	.pipe(gulp.dest("js/dev/"));
+	.pipe(gulp.dest('js'));
 });
 
 
-gulp.task("minifyScripts", function() {
-    gulp.src("js/dev/piped.js")
+gulp.task("minifyScripts", ["concatScripts"], function() {
+return gulp.src("js/piped.js")
       .pipe(uglify())
       .pipe(rename('piped.min.js'))
-      .pipe(gulp.dest('js/dist/'));
+      .pipe(gulp.dest('js'));
 });
 
 gulp.task("compileSass", function() {
-    gulp.src('scss/application.scss')
+return gulp.src('scss/application.scss')
     	.pipe(maps.init())
     	.pipe(sass())
     	.pipe(maps.write('./'))
-    	.pipe(gulp.dest('css/dev/'))
-    	.pipe()
+    	.pipe(gulp.dest('css'))
 });
 
-gulp.task('watchSass', function() {
+gulp.task('watchFiles', function() {
 	gulp.watch('scss/**/*.scss', ['compileSass']);
+	gulp.watch('js/piped.js', ['concatScripts']);
 })
 
-gulp.task('build', ['concatScripts', 'minifyScripts' ]);  // 'compileSass' não está definido para este projeto ainda pois não começamos a usar sass
+gulp.task('clean', function(){
+	del(['dist', 'css/application.css*', 'js/piped*.js*']);
+});
+
+gulp.task('build', ['minifyScripts', 'compileSass' ], function () {
+return gulp.src(['css/application.css', 'js/piped.min.js', 'index.html', 'img/**'], { base:'./'})
+	  .pipe(gulp.dest('dist'));
+}); 
+
+gulp.task('serve', ['watchFiles']);
+
+gulp.task("default",["clean"], function() {
+	gulp.start('build');
+});
 
 
